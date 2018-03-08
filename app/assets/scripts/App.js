28d674a6392c75450814f9d5c4f54605cbff6e9e
Reddel Gastelum <reddelgastelum@gmail.com>
2018-03-08 13:53:44 -0800

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
-var MobileMenu = require('./modules/mobileMenu');
+var MobileMenu = require('./modules/MobileMenu');
+var RevealOnScroll = require('./modules/RevealOnScroll');
+var $ = require('jquery');
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+var $ = require('jquery'),
+waypoints = require('../../../../node_modules/waypoints/lib/noframework.waypoints')
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
+}
+
+module.exports = RevealOnScroll;
